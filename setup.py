3f2c07b3f65b8ec04f829f5e1b95a05377a9959f
harik2361 <119810309+harik2361@users.noreply.github.com>
2022-12-05 00:22:05 +0530

    setup config to enable pip installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="snowflake",
+    version="0.0.1",
+    author="hari",
+    author_email="harikrishnandilliravi@gmail.com",
+    url="https://github.com/harik2361/dsss-ex5",
+    packages=setuptools.find_packages(),
+)
